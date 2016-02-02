25576f56077a946128ae07820c7fca723d51a9f4
Rowan Sandhu <rowan.sandhu@berkeley.edu>
2016-02-02 02:31:29 +0000

    Finished Part 2, Lemon Squeezy

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -25,16 +25,22 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if !(s[0] =~ /[aeiouAEIOU]/)
+    s[0] =~ /[a-zA-Z]/
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+  x = s.to_i(2)
+  x%4 == 0
+  end
 end
+
 
 # Part 3
 
